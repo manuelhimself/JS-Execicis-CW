cde0bfaad2219e688e4826af30b9e4a982fb4f2f
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-11-16 09:45:18 +0100

    added ex25

---
+++ Tema02_JQuery/ex25.php
@@ -0,0 +1,16 @@
+<?php
+$num1=$_REQUEST['num1'];
+$num2=$_REQUEST['num2'];
+$op=$_REQUEST['op'];
+
+if($op == 'add'){
+    echo $num1 + $num2;
+}elseif($op == 'sub'){
+    echo $num1 - $num2;
+}elseif($op == 'mult'){
+    echo $num1 * $num2;
+}elseif($op == 'div'){
+    echo $num1 / $num2;
+}
+
+?>
