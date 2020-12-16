58c5fa87bd9ab23fa4bcc83c5506c11474e3f1a2
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-12-16 19:32:35 +0100

    Afegits els dos darrers exercicis de repas

---
+++ Repàs_Temes1-5/bookstore/llibre.js
@@ -0,0 +1,9 @@
+class llibre {
+    constructor(title, numPages, category, col, row) {
+        this.title = title;
+        this.numPages = numPages;
+        this.category = category;
+        this.col = col;
+        this.row = row;
+    }
+}
